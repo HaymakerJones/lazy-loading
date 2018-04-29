13949dbd6e505a8d0149c8038645ad3cdd00df86
HaymakerJones <js388111@gmail.com>
2018-04-29 09:13:50 -0400

    setup complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'a', component: AComponent },
+  { path: 'b', component: BComponent },
+  { path: '**', component: NotFoundComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NotFoundComponent,
+    AComponent,
+    BComponent
   ],
   imports: [
     BrowserModule,
